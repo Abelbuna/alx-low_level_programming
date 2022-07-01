1ed8b3717a5303c5052efdb1ac3dca1ea712b46c
Abel Mekonen <abelemekonenit@gmail.com>
2022-07-01 10:22:29 -0700

    rmm

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 /**
-<<<<<<< HEAD
 *main - main block
 *Return: 0
 */
-=======
-* main - main block
-* Return: o
-*/
->>>>>>> d2ff178a50b839beadd1521f480a95d30b156ab6
 int main(void)
-{
+{	
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
