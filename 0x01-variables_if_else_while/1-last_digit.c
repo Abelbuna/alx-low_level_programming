218398e3bf53747d8200ad6401158a543b109067
Abel Mekonen <abelemekonenit@gmail.com>
2022-07-01 09:14:52 -0700

    hagose

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+<<<<<<< HEAD
+*main - main block
+*Return: 0
+*/
+=======
+* main - main block
+* Return: o
+*/
+>>>>>>> d2ff178a50b839beadd1521f480a95d30b156ab6
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
