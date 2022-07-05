2670e8cc148227bc7c5bf52675bbb30c0947f621
Abel Mekonen <abelemekonenit@gmail.com>
2022-07-05 05:03:06 -0700

    mi

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+#include "main.h"
+/**
+* main -main block
+* Return: 0
+*/
+int main(void)
+{
+char ch[8] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+{
+_putchar(ch[i]);
+}
+_putchar('\n');
+return (0);
+}
