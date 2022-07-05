8535d236da7a2551ee4f970981bcc01fb5bc6796
Abel Mekonen <abelemekonenit@gmail.com>
2022-07-05 06:48:05 -0700

    kon

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_alphabet - check the code
+* Return: Always 0
+*/
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
