d0d5673ef3acd5fd34c7c38e9222d0bf7c45e79c
Abel Mekonen <abelemekonenit@gmail.com>
2022-07-05 07:22:06 -0700

    henoke

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _abs - check the code.
+* @n: nice
+* Return: Always 0.
+*/
+int _abs(int n)
+{
+if (n > 0)
+return (n);
+else if (n == 0)
+return (0);
+else
+return (-n);
+}
