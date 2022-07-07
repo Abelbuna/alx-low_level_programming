42f6f039e97558af4d7d2e3005095aa213160315
Abel Mekonen <abelemekonenit@gmail.com>
2022-07-07 01:35:20 -0700

    besho

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,6 +8,7 @@
 void print_times_table(int n)
 {
 int row, column, product;
+
 if (n >= 0 && n < 15)
 {
 for (row = 0; row <= n; row++)
@@ -40,6 +41,7 @@ _putchar(' ');
 _putchar(product / 100 + '0');
 _putchar((product / 10) % 10 + '0');
 _putchar(product % 10 + '0');
+}
 }
 _putchar('\n');
 }
