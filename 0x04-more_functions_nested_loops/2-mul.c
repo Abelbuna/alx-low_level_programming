7b553daad06d2dee05efd358751f648127aefb82
Abel Mekonen <abelemekonenit@gmail.com>
2022-07-09 11:44:25 -0700

    bcc

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* mul - check the code.
+ * @a: is an intiger
+ * @b: is an intiger
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+int product;
+product = a * b;
+return (product);
+}
