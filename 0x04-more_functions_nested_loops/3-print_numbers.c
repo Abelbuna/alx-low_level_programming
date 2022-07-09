b1dbab40ddc3a48efc9f42582d1fa067bf5e3943
Abel Mekonen <abelemekonenit@gmail.com>
2022-07-09 11:48:57 -0700

    samri

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_numbers - check the code
+* Return: Always 0
+*/
+void print_numbers(void)
+{
+int n;
+for (n = 48; n < 58; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
