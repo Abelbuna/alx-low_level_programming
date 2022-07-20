c53dc131624d20bafd8c48d3e4f08ad9b99a57f9
Abelbuna <106812613+Abelbuna@users.noreply.github.com>
2022-07-20 09:35:13 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,25 +1,44 @@
+#include "main.h"
 /**
- * wildcmp - compares two strings
- * @s1: 1st string to be compared
- * @s2: second string containing '*' or not
- * Return: 1 if the strings can be considered as identical, and 0
- **/
-int wildcmp(char *s1, char *s2)
+ * _strlen_recursion - size
+ * @s: pointer to string params
+ * Return: recursion
+ */
+int _strlen_recursion(char *s)
 {
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '\0')
-		return (0);
-	if (*s2 == '*')
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-	if (*s1 == '\0')
+	if (!*s)
 	{
-		if (*s2 != '*')
-			return (0);
-		else
-			return (wildcmp(s1, s2 + 1));
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
+/**
+ * p1 - palindrome
+ * @s: pointer to string
+ * @l: position
+ * Return: boolena
+ */
+int p1(char *s, int l)
+{
+	if (l < 1)
+	{
+		return (1);
+	}
+
+	if (*s == *(s + l))
+	{
+		return (p1(s + 1, l - 2));
 	}
 	return (0);
+}
+/**
+ * is_palindrome - palindrome
+ * @s: pointer to string
+ * Return: recursion
+ */
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	return (p1(s, len - 1));
 }
