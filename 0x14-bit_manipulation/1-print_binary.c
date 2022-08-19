6c86ebff862da06eda572d9ac090ffdcbbc4e1e1
Abelbuna <106812613+Abelbuna@users.noreply.github.com>
2022-08-19 10:35:39 +0300

    bim

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *print_binary - prints binary rep of a number
+ *@n:no. to print
+ *Return:void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int mask = 1;
+
+	if (n > 1)
+		print_binary(n >> 1);
+	if ((n & mask) == 1)
+		_putchar('1');
+	else
+		_putchar('0');
+}
